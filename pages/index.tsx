c109f91ccaaef066d85493bf8e7f8a8cb047dfc6
Or Linzer <orlinzer@gmail.com>
2022-07-14 17:04:28 +0300

    Add onsubmit handler

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,43 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
 
-  const firstNameRef = useRef(null);
-  const lastNameRef = useRef(null);
+  // Handles the submit event on form submit.
+  const handleSubmit = async (event: { preventDefault: () => void; target: { first: { value: any }; last: { value: any } } }) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault()
+
+    // Get data from the form.
+    const data = {
+      first: event.target.first.value,
+      last: event.target.last.value,
+    };
+
+    // Send the data to the server in JSON format.
+    const JSONdata = JSON.stringify(data);
+
+    // API endpoint where we send form data.
+    const endpoint = '/api/set_user';
+
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: 'POST',
+      // Tell the server we're sending JSON.
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    };
+
+    // Send the form data to our forms API on Vercel and get a response.
+    const response = await fetch(endpoint, options);
+
+    // Get the response data from server as JSON.
+    // If server returns the name submitted, that means the form works.
+    const result = await response.json();
+    alert(`Is this your full name: ${result.first} ${result.last}`);
+  }
 
   return (
     <div className={styles.container}>
@@ -24,13 +59,15 @@ const Home: NextPage = () => {
       </header>
 
       <main className={styles.main}>
-        <form action="/api/set_user" method="post">
-          {/* <label htmlFor={firstNameRef}>First name:</label>
-          <input type="text" ref={firstNameRef} name="first" /> */}
-          <label >First name:</label>
-          <input type="text" name="first" />
-          <label >Last name:</label>
-          <input type="text" name="last" />
+        {/* <form action="/api/set_user" method="post"> */}
+        <form onSubmit={handleSubmit}>
+
+          <label htmlFor='first'>First name:</label>
+          <input type="text" id='first' name="first" required />
+
+          <label htmlFor='last'>Last name:</label>
+          <input type="text" id='last' name="last" required />
+
           <button type="submit">Submit</button>
         </form>
       </main>
